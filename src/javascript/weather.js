d1b7bd975586db1f7311e73d535c87866877339f
Jordan <jordansampson23@gmail.com>
2023-02-15 11:49:38 -0600

    getWeather by zipcode

--- src/javascript/weather.js
+++ src/javascript/weather.js
@@ -1,17 +1,24 @@
 
 
 const weather = (() => {
+    let weatherData;
 
-    async function getWeather(location) {
-        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Slaughters,US,ky&APPID=4eee9c6ebad320d7c297db4525d20db9&units=imperial', {mode: 'cors'})
-        const weatherData = await response.json()
-        console.log(weatherData)
+    async function getWeather(zipcode) {
+        try {
+            const response = await fetch('https://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + '&APPID=4eee9c6ebad320d7c297db4525d20db9&units=imperial', {mode: 'cors'})
+            let weatherData = await response.json()
+            console.log(weatherData)
+        } catch (err) {
+            console.error(err)
+            
+        }
     }
 
 
 
     return {
-        getWeather
+        getWeather,
+        weatherData
     }
 
 })();
