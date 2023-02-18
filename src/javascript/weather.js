1e1f47dfb7dfb118b6df939d26e8dea0f844b564
Jordan <jordansampson23@gmail.com>
2023-02-18 12:46:14 -0600

    getForecast fn added

--- src/javascript/weather.js
+++ src/javascript/weather.js
@@ -2,6 +2,7 @@ import dom from "./dom";
 
 const weather = (() => {
     let weatherData;
+    let forecastData;
 
     async function getWeather(zipcode) {
         try {
@@ -9,17 +10,37 @@ const weather = (() => {
             let weatherData = await response.json()
             console.log(weatherData)
             dom.displayCurrentWeather(weatherData)
+            dom.revealCurrentWeather()
+            getForecast(zipcode)
         } catch (err) {
             console.error(err)
             // Throw error to user
         }
     }
 
+    async function getWeatherIcon(weatherData) {
+        const iconCode = weatherData.weather.icon;
+        
+        // Where will I call img.src?
+        // When will I dynamically create img?
+        // When will I call this function?
+    }
+
+
+    async function getForecast(zipcode) {
+        const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?zip=' + zipcode + '&APPID=4eee9c6ebad320d7c297db4525d20db9&units=imperial')
+        let forecastData = await response.json()
+        console.log(forecastData)
+        dom.displayForecast(forecastData)
+    }
+
 
 
     return {
         getWeather,
-        weatherData
+        weatherData,
+        forecastData,
+        getForecast
     }
 
 })();
